2e82deddf49c577b3d836cddd18ebf89b675c4bd
root <root@kashirinas.fvds.ru>
2017-09-18 03:22:43 -0400

    init grabber app

--- cca/settings.py
+++ cca/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '8(bz@t3(ss#ik70v^11_z2lv7izu2rqr*mwv0zkdevtrg_1kme'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,18 +69,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cca.wsgi.application'
-
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cca',
+        'USER': 'kas',
+        'PASSWORD': 'kas',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
